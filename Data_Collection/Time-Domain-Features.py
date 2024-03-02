4cc87684b99cdebda819327f1c9775eddde73376
Joseph P. Vasquez <85473072+Lxfted73@users.noreply.github.com>
2024-03-02 16:07:29 +0800

    Summary: Fixed File pathing for Time-Domain-Features.py

--- Data_Collection/Time-Domain-Features.py
+++ Data_Collection/Time-Domain-Features.py
@@ -39,7 +39,7 @@ print("RMS:\n", rms_values)
 pd.set_option('display.max_columns', None)
 
 #2
-df_test_spectogram=pd.read_parquet("test_spectogram.parquet")
+df_test_spectogram=pd.read_parquet("Data/test_spectogram.parquet")
 mean_values=df_test_spectogram.mean()
 print("Mean Values:\n", mean_values)
 
@@ -58,7 +58,7 @@ print("\n\nRMS:\n", rms_values)
 
 
 #3
-df_train_eegs=pd.read_parquet("train_eegs.parquet")
+df_train_eegs=pd.read_parquet("Data/train_eegs.parquet")
 
 mean_values = df_train_eegs.mean()
 print("Mean Values:\n", mean_values)
@@ -78,7 +78,7 @@ print("\n\nRMS:\n", rms_values)
 
 
 #4
-df_train_spectogram=pd.read_parquet("train_spectogram.parquet")
+df_train_spectogram=pd.read_parquet("Data/train_spectogram.parquet")
 mean_values = df_train_spectogram.mean()
 print("Mean Values:\n", mean_values)
 
@@ -95,41 +95,41 @@ print("Standard Deviation:\n", std_values)
 print("\n\nVariance:\n", variance_values)
 print("\n\nRMS:\n", rms_values)
 
-
-
-
-
-
-pd.set_option('display.max_columns', None)
-
-# print("df_train_eegs.shape", df_test_eegs.shape)
-# print("df_train_eegs.head()\n", df_test_eegs.head)
-
-electrodes = df_test_eegs.columns[:-1]  # Exclude the last column (EKG)
-electrode_voltages = df_test_eegs[electrodes]
-print("Electrodes: \n", electrodes,"Electrode Locations: \n", electrode_voltages)
-
-# Extract signal amplitude features
-signal_amplitudes_mean = df_test_eegs[electrodes].mean(axis=0)
-signal_amplitudes_max = df_test_eegs[electrodes].max(axis=0)
-signal_amplitudes_min = df_test_eegs[electrodes].min(axis=0)
-signal_amplitudes_variance = df_test_eegs[electrodes].var(axis=0)
-
-df_test_eegs['Mean_Amplitude'] = signal_amplitudes_mean.values
-df_test_eegs['Max_Amplitude'] = signal_amplitudes_max.values
-df_test_eegs['Min_Amplitude'] = signal_amplitudes_min.values
-df_test_eegs['Variance_Amplitude'] = signal_amplitudes_variance.values
-
-# Print the updated DataFrame
-print(df_test_eegs)
-
-# # Print the calculated features
-# print("Mean Signal Amplitudes:")
-# print(signal_amplitudes_mean)
-# print("\nMaximum Signal Amplitudes:")
-# print(signal_amplitudes_max)
-# print("\nMinimum Signal Amplitudes:")
-# print(signal_amplitudes_min)
-# print("\nSignal Amplitudes Variance:")
-# print(signal_amplitudes_variance)
+#
+#
+#
+#
+#
+# pd.set_option('display.max_columns', None)
+#
+# # print("df_train_eegs.shape", df_test_eegs.shape)
+# # print("df_train_eegs.head()\n", df_test_eegs.head)
+#
+# electrodes = df_test_eegs.columns[:-1]  # Exclude the last column (EKG)
+# electrode_voltages = df_test_eegs[electrodes]
+# print("Electrodes: \n", electrodes,"Electrode Locations: \n", electrode_voltages)
+#
+# # Extract signal amplitude features
+# signal_amplitudes_mean = df_test_eegs[electrodes].mean(axis=0)
+# signal_amplitudes_max = df_test_eegs[electrodes].max(axis=0)
+# signal_amplitudes_min = df_test_eegs[electrodes].min(axis=0)
+# signal_amplitudes_variance = df_test_eegs[electrodes].var(axis=0)
+#
+# df_test_eegs['Mean_Amplitude'] = signal_amplitudes_mean.values
+# df_test_eegs['Max_Amplitude'] = signal_amplitudes_max.values
+# df_test_eegs['Min_Amplitude'] = signal_amplitudes_min.values
+# df_test_eegs['Variance_Amplitude'] = signal_amplitudes_variance.values
+#
+# # Print the updated DataFrame
+# print(df_test_eegs)
+#
+# # # Print the calculated features
+# # print("Mean Signal Amplitudes:")
+# # print(signal_amplitudes_mean)
+# # print("\nMaximum Signal Amplitudes:")
+# # print(signal_amplitudes_max)
+# # print("\nMinimum Signal Amplitudes:")
+# # print(signal_amplitudes_min)
+# # print("\nSignal Amplitudes Variance:")
+# # print(signal_amplitudes_variance)
 
